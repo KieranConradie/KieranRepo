f4709eeaefa1d816beef6ad6a110e1bc1ac1e735
Kieran <kieran@al.co.za>
2023-02-01 17:19:21 +0200

    changes

--- after_install.sh
+++ after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws s3 cp s3://kieranartifactsbucket/KieranPipeLine/KieranCDst/index.html /usr/share/nginx/html/index.html
+aws s3 cp s3://kieranartifactsbucket/index.html /usr/share/nginx/html/index.html
 sudo chmod 644 /usr/share/nginx/html/index.html
 sudo service nginx restart
 
