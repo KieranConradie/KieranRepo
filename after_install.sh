c68004425a415c189d38c23808fab3140fe3fb69
Kieran <kieran@al.co.za>
2023-02-01 14:52:01 +0200

    changes

--- after_install.sh
+++ after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo rm /usr/share/nginx/html/index.html
-sudo cp KieranCDstore/DemoWebsite.html /usr/share/nginx/html/index.html
+sudo cp KieranCDstore/index.html /usr/share/nginx/html/index.html
 sudo service nginx restart
 
